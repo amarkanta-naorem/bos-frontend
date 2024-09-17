b791365eed0863fc3f5264e2a1d945a2d1b4e5f2
amarkanta-naorem <amarkanta.meitei.naorem@gmail.com>
2024-09-17 13:18:25 +0530

    Customized sidebar color and hover effect

--- src/components/Dashboard/Body/Courses/index.js
+++ src/components/Dashboard/Body/Courses/index.js
@@ -5,7 +5,7 @@ import Table from "./table";
 const Course = () => {
   return (
     <Fragment>
-      <div className="w-[86.5rem] md:w-full h-[86vh] overflow-x-scroll no-scrollbar bg-gray-100">
+      <div className="w-[86.5rem] md:w-full h-[86vh] overflow-x-scroll no-scrollbar bg-stone-100">
         <div className="flex justify-between items-center px-5 md:px-10 pt-3 md:pt-5">
           <h1 className="text-2xl font-extrabold text-teal-950">Courses</h1>
           <p className="text-xs text-teal-950">Dashboard / Courses</p>
