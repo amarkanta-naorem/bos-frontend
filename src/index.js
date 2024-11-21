53bc88c3a8180bce010365ceb0ae76316e8323ac
amarkanta-naorem <amarkanta.meitei.naorem@gmail.com>
2024-11-21 13:17:53 +0530

    Bug Fix - Context API

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import Routes from "./router/routes";
 
+// This is to conditionally apply React.StrictMode only in development
+const isDevelopment = process.env.NODE_ENV === "development";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={Routes} />
-  </React.StrictMode>
+    isDevelopment ? (
+        <React.StrictMode>
+            <RouterProvider router={Routes} />
+        </React.StrictMode>
+    ) : (
+        <RouterProvider router={Routes} />
+    )
 );
 
 // If you want to start measuring performance in your app, pass a function
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // your app source files
+    "./node_modules/@tailwindcss/**/*.{js,jsx,ts,tsx}", // third-party libraries (if needed)
+  ],
   theme: {
     extend: {},
   },
